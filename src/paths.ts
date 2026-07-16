8894637017e1731851594e462bca92fe26be5389
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-07-16 19:26:26 -0400

    fix: root dir path

--- src/paths.ts
+++ src/paths.ts
@@ -1,8 +1,8 @@
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
-export const SCRIPT_DIR = dirname(fileURLToPath(import.meta.url));
-export const PROCESSED_ASSETS_DIR = join(SCRIPT_DIR, "processed-assets");
-export const BUILD_DIR = join(SCRIPT_DIR, "build");
-export const NANOEMOJI_BIN_DIR = join(SCRIPT_DIR, "nanoemoji-env", "bin");
+export const ROOT_DIR = dirname(dirname(fileURLToPath(import.meta.url)));
+export const PROCESSED_ASSETS_DIR = join(ROOT_DIR, "processed-assets");
+export const BUILD_DIR = join(ROOT_DIR, "build");
+export const NANOEMOJI_BIN_DIR = join(ROOT_DIR, "nanoemoji-env", "bin");
 export const NANOEMOJI_BIN = join(NANOEMOJI_BIN_DIR, "nanoemoji");
